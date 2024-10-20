79772292dc24a4ceb4b1ca2a78a3571ab1cd9554
Shirosensei <yeomingen02@gmail.com>
2024-10-20 19:06:08 +0800

    latest

--- project/src/main/java/cs204/project/ProjectApplication.java
+++ project/src/main/java/cs204/project/ProjectApplication.java
@@ -6,50 +6,52 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.jdbc.core.JdbcTemplate;
 import io.github.cdimascio.dotenv.Dotenv;
 
-import cs204.project.tournament.T
+import cs204.project.tournament.TournamentRepository;
 
-  
-    port java.util.List;
-    port java.util.ArrayList;
-import .util.Arrays;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.time.LocalDate;
 
-    blic class ProjectApplication {
+@SpringBootApplication
+public class ProjectApplication {
 
-    ublic static void main(String[] args) {
-    // 
-     Dotenv dotenv = Dotenv.configure().load();
+  public static void main(String[] args) {
+    //
+    Dotenv dotenv = Dotenv.configure().load();
     System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
 
-nContext ctx = SpringApplication.run(ProjectApplication.class, args);
+    ApplicationContext ctx = SpringApplication.run(ProjectApplication.class, args);
 
     JdbcTemplate template = ctx.getBean(JdbcTemplate.class);
     // TournamentRepository repo = ctx.getBean(TournamentRepository.class);
 
     template.execute("CREATE TABLE IF NOT EXISTS tournaments (" +
-                 "id BIGSERIAL PRIMARY KEY," +
-                 "name VARCHAR(255) NOT NULL," +
-                 "date DATE NOT NULL," +
-                 "rankRange INT[] NOT NULL," +
-                 "status VARCHAR(50) NOT NULL," +
-                 "region VARCHAR(100) NOT NULL," +
-                 "playerList JSON," +
-                 "round INT" +
-                 ")");
-  
+        "id BIGSERIAL PRIMARY KEY," +
+        "name VARCHAR(255) NOT NULL," +
+        "date DATE NOT NULL," +
+        "rankRange INT[] NOT NULL," +
+        "status VARCHAR(50) NOT NULL," +
+        "region VARCHAR(100) NOT NULL," +
+        "playerList JSON," +
+        "round INT" +
+        ")");
+
     // List<Long> playerList = new ArrayList<>();
     // for (int i = 1; i <= 32; i++){
-    //   playerList.add((long)i);
+    // playerList.add((long)i);
     // }
 
     // List<Tournament> listTournaments = Arrays.asList(
-    //   new Tournament("t1", LocalDate.of(2024, 9, 23), new int[]{0,2000}, "Open", "Asia", playerList, 1),
-    //   new Tournament("t2", LocalDate.of(2024, 9, 23), new int[]{0,1000}, "Open", "America", playerList, 1)
+    // new Tournament("t1", LocalDate.of(2024, 9, 23), new int[]{0,2000}, "Open",
+    // "Asia", playerList, 1),
+    // new Tournament("t2", LocalDate.of(2024, 9, 23), new int[]{0,1000}, "Open",
+    // "America", playerList, 1)
     // );
 
     // listTournaments.forEach(tournament -> {
-    //   repo.save(tournament);
+    // repo.save(tournament);
     // });
-	}
+  }
 
 }

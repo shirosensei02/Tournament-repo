09ba113ad0e1615bd51e5b4c19345687a1f9599d
Malcolm <malcolm.limhw@gmail.com>
2024-11-12 22:02:47 +0800

    Deleted unused TournamentNotFoundException

--- project/src/main/java/cs204/project/Controller/GlobalExceptionHandler.java
+++ project/src/main/java/cs204/project/Controller/GlobalExceptionHandler.java
@@ -1,4 +1,4 @@
-package cs204.project.Exception;
+package cs204.project.Controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -6,6 +6,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+
+import cs204.project.Exception.TournamentNotFoundException;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -22,6 +24,4 @@ public class GlobalExceptionHandler {
       String errorMessage = "An unexpected error occurred.";
       return new ResponseEntity<>(errorMessage, HttpStatus.INTERNAL_SERVER_ERROR);
   }
-
-  // You can add more exception handlers for other exceptions
 }
